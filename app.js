311638633207b87157c5f37072cfe1e52b741c53
Sufyan Ghazali <sufyan.ghazali@gmail.com>
2020-09-14 18:57:37 +0800

    Add routes and templates

--- app.js
+++ app.js
@@ -3,31 +3,31 @@ const express = require("express"),
     PORT = process.env.PORT || 4000,
     mongoose = require("mongoose");
 
-
-
 // Require routes
 const indexRoutes = require("./routes/index");
 const shopRoutes = require("./routes/shops");
 
 // Connect to database
-const url = process.env.DATABASEURL || "localhost/filtered";
+const url = process.env.DATABASEURL || "mongodb://localhost/filtered";
+
+console.log(process.env);
 mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
-    console.log("Connected to Filtered database");
+    console.log(`Connected to ${url}`);
 }).catch(err => {
     console.log(`ERROR: ${err}`);
 })
 
-
+app.use(express.urlencoded({extended: true})); // replaces body-parser. parse url-encoded bodies
 app.set("view engine", "ejs"); // need to download
 app.use(express.static("public"));
 
-
+// Routing
 app.use(indexRoutes);
 app.use("/shops", shopRoutes);
 
 app.listen(PORT, (req, res) => {
     console.log("Server started. Yeet yeet!");
-})
+});
--- routes/shops.js
+++ routes/shops.js
@@ -2,18 +2,61 @@ const express = require("express");
 const router = express.Router();
 const Shop = require("../models/shop");
 
+// INDEX - show all shops. route is "/shops"
 router.get("/", (req, res) => {
-
     // Get list of shops from db
     Shop.find({}, (err, shops) => {
         if (err) {
             console.log(err);
         } else {
-            res.render("shop/index", {shops: shops}); // Passing shops array to template
+            console.log("hi");
+            res.render("shops/index", {shops: shops}); // Passing shops array to template
         }
     })
 });
 
+// NEW - show form to create new shop
+router.get("/new", (req, res) => {
+    // should check if user is logged in first
+    res.render("shops/new");
+})
 
+// CREATE - add new shop
+router.post("/", (req, res) => {
+    // need isLoggedIn middleware
+    // create new shop from request body
+    const newShop = req.body.shop;
+
+    Shop.create(newShop, (err, shop) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Created new shop");
+            console.log(shop);
+            res.redirect("/shops");
+        }
+    });
+});
+
+// SHOW - profile for a specific shop
+router.get("/:id", (req, res) => {
+    const id = req.params.id;
+
+    // Get shop by id
+    Shop.findById(id, (err, shop) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("shops/show", {shop: shop});
+        }
+    });
+});
+
+
+// EDIT - edit shop page
+
+// UPDATE - update shop logic
+
+// DESTROY - yeet a shop
 
 module.exports = router;
